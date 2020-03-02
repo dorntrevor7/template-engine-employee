4a25cffcfb13017f1483c7c6935b4f4bff9c3a7c
Trevor Dorn <dorntrevor7@gmail.com>
2020-03-02 14:20:50 -0700

    employees working w/out hardcode

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -2,7 +2,7 @@ const Employee = require("../lib/Employee");
 
 test("Can instantiate Employee instance", () => {
   const e = new Employee();
-  expect(typeof(e)).toBe("object");
+  expect(typeof (e)).toBe("object");
 });
 
 test("Can set name via constructor arguments", () => {
